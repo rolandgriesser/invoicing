b71adf1270c7a24164adb4eb07c22c2373808d88
Roland Grießer <office@rolandgriesser.at>
2019-03-10 10:41:35 +1100

    Changed user id to Guid and added navigation properties

--- src/server/Models/User.cs
+++ src/server/Models/User.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace invoicing.server.Models
 {
-    public class User : IdentityUser
+    public class User : IdentityUser<Guid>
     {
+        public Guid OrganizationId { get; set; }
+        public Organization Organization { get; set; }
+
     }
 }
