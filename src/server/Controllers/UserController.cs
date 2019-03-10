90cbf56d8faf74d3d2c423d6ef3744852e8bebfe
Roland Grießer <office@rolandgriesser.at>
2019-03-10 11:11:27 +1100

    Added organization to the registration route

--- src/server/Controllers/UserController.cs
+++ src/server/Controllers/UserController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using invoicing.server.Data;
 using invoicing.server.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace invoicing.server.Controllers
@@ -15,18 +17,23 @@ namespace invoicing.server.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
+        private readonly InvoicingDbContext _dbContext;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly IEmailSender _emailSender;
         public UserController(UserManager<User> userManager,
+            InvoicingDbContext dbContext,
             SignInManager<IdentityUser> signInManager,
             IEmailSender emailSender)
         {
             _userManager = userManager;
+            _dbContext = dbContext;
             _signInManager = signInManager;
             _emailSender = emailSender;
         }
         
         public class RegisterDto {
+            public Guid? OrganizationId { get; set; }
+            public string OrganizationName { get; set; }
             public string Email { get; set; }
             public string Password { get; set; }
         }
@@ -40,7 +47,39 @@ namespace invoicing.server.Controllers
         [AllowAnonymous] 
         public async Task<ActionResult<RegisterResultDto>> Register([FromBody]RegisterDto register)
         {
-            var user = new User { UserName = register.Email, Email = register.Email };
+            Organization organization = null;
+            if(register.OrganizationId.HasValue) 
+            {
+                //todo get organization of current logged in user
+                // organization = _dbContext.Organizations.FirstOrDefault(i => i.Id == regi);
+            }
+            else 
+            {
+                if(string.IsNullOrEmpty(register.OrganizationName)) {
+                    return new RegisterResultDto() {
+                        Success = false,
+                        Errors = new IdentityError[] {
+                            new IdentityError() { Code = "OrganizationNameEmpty", Description = "OrganizationName can't be empty."}
+                        }
+                    };
+                }
+                organization = new Organization() {
+                    Name = register.OrganizationName
+                };
+                _dbContext.Organizations.Add(organization);
+                _dbContext.SaveChanges();
+            }
+
+            if(organization == null) {
+                return new RegisterResultDto() {
+                    Success = false,
+                    Errors = new IdentityError[] {
+                        new IdentityError() { Code = "OrganizationNull", Description = "The organization of the user couldn't be fetched or created."}
+                    }
+                };
+            }
+
+            var user = new User { UserName = register.Email, Email = register.Email, Organization = organization };
             var result = await _userManager.CreateAsync(user, register.Password);
 
             if (result.Succeeded)
@@ -65,7 +104,7 @@ namespace invoicing.server.Controllers
         }
 
         [HttpGet("confirm")] 
-        public async Task<ActionResult<bool>> ConfirmEmail() {
+        public async Task<ActionResult<bool>> ConfirmEmail(string user) {
             return true;
         }
     }
